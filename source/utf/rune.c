71188357ccc74a381f48d4747a2e4a8fedb1626b
Mike D. Lowis <mike.lowis@gentex.com>
2015-06-09 11:44:46 -0400

    Fixed compiler warnings for stubs

--- source/utf/rune.c
+++ source/utf/rune.c
@@ -2,26 +2,35 @@
 
 int runetochar(char* s, Rune* r)
 {
+    (void)s;
+    (void)r;
     return 0;
 }
 
 int chartorune(Rune* r, char* s)
 {
+    (void)r;
+    (void)s;
     return 0;
 }
 
 int runelen(long r)
 {
+    (void)r;
     return 0;
 }
 
 int runenlen(Rune* r, int num)
 {
+    (void)r;
+    (void)num;
     return 0;
 }
 
 bool fullrune(char* s, int n)
 {
+    (void)s;
+    (void)n;
     return false;
 }
 
--- source/utf/runetype.c
+++ source/utf/runetype.c
@@ -2,57 +2,67 @@
 
 Rune tolowerrune(Rune ch)
 {
+    (void)ch;
     return 0;
 }
 
 Rune totitlerune(Rune ch)
 {
+    (void)ch;
     return 0;
 }
 
 Rune toupperrune(Rune ch)
 {
+    (void)ch;
     return 0;
 }
 
 Rune tobaserune(Rune ch)
 {
+    (void)ch;
     return 0;
 }
 
 bool isalpharune(Rune ch)
 {
+    (void)ch;
     return false;
 }
 
 bool isbaserune(Rune ch)
 {
+    (void)ch;
     return false;
 }
 
 bool isdigitrune(Rune ch)
 {
+    (void)ch;
     return false;
 }
 
 bool islowerrune(Rune ch)
 {
+    (void)ch;
     return false;
 }
 
 bool isspacerune(Rune ch)
 {
+    (void)ch;
     return false;
 }
 
 bool istitlerune(Rune ch)
 {
+    (void)ch;
     return false;
 }
 
 bool isupperrune(Rune ch)
 {
+    (void)ch;
     return false;
 }
-
 
