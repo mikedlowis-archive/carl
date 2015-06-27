cd1b7c68ae3edb29b0911cdea32533d5159904b2
Michael D. Lowis <mike@mdlowis.com>
2015-06-27 16:38:58 -0400

    Added binary search tree implementation to the data structures folder

--- source/data/bstree.h
+++ source/data/bstree.h
@@ -4,17 +4,22 @@
 #ifndef BSTREE_H
 #define BSTREE_H
 
+#include <libc.h>
+
 typedef struct bstree_node_t {
-    struct bstree_node_t* parent;
     struct bstree_node_t* left;
     struct bstree_node_t* right;
 } bstree_node_t;
 
+typedef int (*bstree_cmpfn_t)(bstree_node_t* a, bstree_node_t* b);
+
 typedef struct {
     bstree_node_t* root;
+    bstree_cmpfn_t cmpfn;
+    bool allow_dups;
 } bstree_t;
 
-void bstree_init(bstree_t* bstree);
+void bstree_init(bstree_t* bstree, bstree_cmpfn_t cmpfn, bool allow_dups);
 
 bool bstree_empty(bstree_t* bstree);
 
@@ -23,13 +28,5 @@ size_t bstree_size(bstree_t* bstree);
 void bstree_insert(bstree_t* bstree, bstree_node_t* node);
 
 bstree_node_t* bstree_lookup(bstree_t* bstree, bstree_node_t* node);
-
-bool bstree_node_hasnext(bstree_node_t* node);
-
-bstree_node_t* bstree_node_next(bstree_node_t* node);
-
-bool bstree_node_hasprev(bstree_node_t* node);
-
-bstree_node_t* bstree_node_prev(bstree_node_t* node);
 
 #endif /* BSTREE_H */
--- tests/main.c
+++ tests/main.c
@@ -7,5 +7,6 @@ void main(int argc, char** argv)
     (void)argv;
     RUN_EXTERN_TEST_SUITE(RefCount);
     RUN_EXTERN_TEST_SUITE(SList);
+    RUN_EXTERN_TEST_SUITE(BSTree);
     exit(PRINT_TEST_RESULTS());
 }
